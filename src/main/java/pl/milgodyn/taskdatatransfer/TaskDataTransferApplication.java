25ce89b779b0572852666cd579ec9cfabf234ad1
Miłosz Godyń <milgodyn@outlook.com>
2023-12-11 18:42:08 +0100

    Initial commit

---
+++ src/main/java/pl/milgodyn/taskdatatransfer/TaskDataTransferApplication.java
@@ -0,0 +1,13 @@
+package pl.milgodyn.taskdatatransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDataTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskDataTransferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/milgodyn/taskdatatransfer/TaskDataTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milgodyn.taskdatatransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDataTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
