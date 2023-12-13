e249980392eff2efa6eff99bcea8bef81c4c7e46
Miłosz Godyń <milgodyn@outlook.com>
2023-12-13 19:03:29 +0100

    Update smoke test

--- src/test/java/pl/milgodyn/taskdatatransfer/SmokeTest.java
+++ src/test/java/pl/milgodyn/taskdatatransfer/SmokeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.milgodyn.taskdatatransfer.application.rest.CoordinatesController;
+import pl.milgodyn.taskdatatransfer.domain.service.CityService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,9 +14,13 @@ class SmokeTest {
     @Autowired
     private CoordinatesController coordinatesController;
 
+    @Autowired
+    private CityService cityService;
+
     @Test
     void contextLoads() {
         assertThat(coordinatesController).isNotNull();
+        assertThat(cityService).isNotNull();
     }
 
 }
