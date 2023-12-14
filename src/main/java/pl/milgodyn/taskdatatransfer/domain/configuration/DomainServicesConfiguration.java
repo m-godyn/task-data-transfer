25fd408b3dbaec2b4fcec8a3ef498cfbb6016df6
Miłosz Godyń <milgodyn@outlook.com>
2023-12-14 19:59:46 +0100

    Connect whole flow

--- src/main/java/pl/milgodyn/taskdatatransfer/domain/configuration/DomainServicesConfiguration.java
+++ src/main/java/pl/milgodyn/taskdatatransfer/domain/configuration/DomainServicesConfiguration.java
@@ -3,13 +3,16 @@ package pl.milgodyn.taskdatatransfer.domain.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.milgodyn.taskdatatransfer.application.mapper.CoordinatesResponseMapper;
+import pl.milgodyn.taskdatatransfer.domain.service.CapitalCityService;
 import pl.milgodyn.taskdatatransfer.domain.service.CityService;
+import pl.milgodyn.taskdatatransfer.domain.service.CoordinatesService;
 
 @Configuration
 public class DomainServicesConfiguration {
 
     @Bean
-    public CityService cityService(CoordinatesResponseMapper coordinatesResponseMapper) {
-        return new CityService(coordinatesResponseMapper);
+    public CityService cityService(CoordinatesResponseMapper coordinatesResponseMapper, CapitalCityService capitalCityService,
+                                   CoordinatesService coordinatesService) {
+        return new CityService(coordinatesResponseMapper, capitalCityService, coordinatesService);
     }
 }
--- src/main/java/pl/milgodyn/taskdatatransfer/domain/service/CityService.java
+++ src/main/java/pl/milgodyn/taskdatatransfer/domain/service/CityService.java
@@ -1,26 +1,20 @@
 package pl.milgodyn.taskdatatransfer.domain.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import pl.milgodyn.taskdatatransfer.application.response.CoordinatesResponse;
 import pl.milgodyn.taskdatatransfer.application.mapper.CoordinatesResponseMapper;
+import pl.milgodyn.taskdatatransfer.application.response.CoordinatesResponse;
 import pl.milgodyn.taskdatatransfer.domain.model.Coordinates;
-
-import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 public class CityService {
 
     private final CoordinatesResponseMapper coordinatesResponseMapper;
+    private final CapitalCityService capitalCityService;
+    private final CoordinatesService coordinatesService;
 
     public CoordinatesResponse getFullInformation(String countryCode) {
-        return coordinatesResponseMapper.mapToResponse(
-                Coordinates.builder()
-                        .capital("Warszawa")
-                        .latitude(BigDecimal.valueOf(52.2319581))
-                        .longitude(BigDecimal.valueOf(21.0067249))
-                        .country("Polska")
-                        .displayName("Warszawa, województwo mazowieckie, Polska")
-                        .build());
+        String capitalCity = capitalCityService.getCapitalCityByCountryCode(countryCode);
+        Coordinates coordinatesByCapitalCity = coordinatesService.getCoordinatesByCapitalCity(capitalCity);
+        return coordinatesResponseMapper.mapToResponse(coordinatesByCapitalCity);
     }
 }
